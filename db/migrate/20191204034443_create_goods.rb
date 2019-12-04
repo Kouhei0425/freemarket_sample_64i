dbf49e8030dd662965b3628432ca094e368b719d
Hiropey <hiropee.yam@gmail.com>
2019-12-04 13:17:00 +0900

    goods-model

--- db/migrate/20191204034443_create_goods.rb
+++ db/migrate/20191204034443_create_goods.rb
@@ -2,6 +2,16 @@ class CreateGoods < ActiveRecord::Migration[5.0]
   def change
     create_table :goods do |t|
 
+      t.string      :name,          null: false
+      t.string      :size,          null: false
+      t.integer     :price,         null: false
+      t.text        :method,        null: false
+      t.text        :ship,          null: false
+      t.text        :explain,       null: false
+      t.references  :category,      null: false, foreign_key: true
+      t.references  :user,          null: false, foreign_key: true
+      t.references  :brand,         null: false, foreign_key: true
+      t.references  :area,          null: false, foreign_key: true
       t.timestamps
     end
   end
