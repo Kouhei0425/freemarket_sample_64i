dfc2e6cb89c5ed58b725b318ffd0a771455af918
hibiki20600 <hibiki_inobe@icloud.com>
2019-12-05 11:25:50 +0900

    Update 20191204034522_create_images.rb

--- db/migrate/20191204034522_create_images.rb
+++ db/migrate/20191204034522_create_images.rb
@@ -3,7 +3,6 @@ class CreateImages < ActiveRecord::Migration[5.0]
     create_table :images do |t|
       t.string       :image,        null: false
       t.references   :good,         null: false, foreign_key: true
-      t.timestamps   
-    end
+      t.timestamps
   end
 end
