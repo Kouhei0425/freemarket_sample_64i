e785eb42f9187a46a4aa015ac119cc9d46966d58
hibiki20600 <hibiki_inobe@icloud.com>
2019-12-05 16:19:54 +0900

    deviseのビュー追加、コントローラーの修正、データの初期値

--- app/controllers/toppage_controller.rb
+++ app/controllers/toppage_controller.rb
@@ -1,4 +1,11 @@
 class ToppageController < ApplicationController
   def index
   end
+
+  def search
+    @goods = Good.search(params[:keyword])
+    @category = Category.search(params[:keyword])
+    @brand_goods = Brand.search(params[:keyword])
+
+  end
 end
--- app/models/good.rb
+++ app/models/good.rb
@@ -4,5 +4,9 @@ class Good < ApplicationRecord
   belongs_to :area
   has_many :category, through: :category_goods
   has_many :images
+  
+  def self.search(keyword)
+    Good.where('name LIKE(?)', "%#{keyword}%")
+  end
 
 end
