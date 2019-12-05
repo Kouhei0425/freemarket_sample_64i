eea554e37fcdc27bdca2b6ca6a2a808e537341ee
hibiki20600 <hibiki_inobe@icloud.com>
2019-12-05 18:58:24 +0900

    make seed to make default Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,17 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the rails dbseed command (or created alongside the database with dbsetup).
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-#   Title.create(:name => '観察日記', :sales_date => '2011-11-14', :price => 1000)
+
+Brand.create(:name => 'シャネル')
+Brand.create(:name => 'ナイキ')
+Brand.create(:name => 'ルイ　ヴィトン')
+Brand.create(:name => 'シュプリーム')
+Brand.create(:name => 'アディダス')
+
 Category.create(:name => 'メンズ')
 Category.create(:name => 'レディース')
 Category.create(:name => 'ベビー・キッズ')
@@ -19,9 +25,3 @@ Category.create(:name => 'ハンドメイド')
 Category.create(:name => 'チケット')
 Category.create(:name => '自動車・オートバイ')
 Category.create(:name => 'その他')
-
-Brand.create(:name => 'シャネル')
-Brand.create(:name => 'ナイキ')
-Brand.create(:name => 'ルイ　ヴィトン')
-Brand.create(:name => 'シュプリーム')
-Brand.create(:name => 'アディダス')
