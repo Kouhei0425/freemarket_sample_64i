4fd083cc843c37556ef72f4d09b2c5e625204554
hibiki20600 <hibiki_inobe@icloud.com>
2019-12-09 17:24:14 +0900

    edit README

--- app/models/good.rb
+++ app/models/good.rb
@@ -2,8 +2,9 @@ class Good < ApplicationRecord
   belongs_to :user
   belongs_to :brand
   belongs_to :area
-  has_many :category, through: :category_goods
-  has_many :images
+  has_many :category, through: :category_goods, dependent: :destroy
+  has_many :images, dependent: :destroy
+  accepts_nested_attributes_for :images
   mount_uploader :image, ImageUploader
   
   def self.search(keyword)
