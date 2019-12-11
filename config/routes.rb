24900176701a6068d17eb09147a25d1e0aef0ccc
hibiki20600 <hibiki_inobe@icloud.com>
2019-12-11 14:06:12 +0900

    make sign_up controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :sessions      => "users/sessions",
+    :registrations => "users/registrations",
+    :passwords     => "users/passwords"
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "toppage#index"
   resources :toppage do
@@ -8,9 +12,16 @@ Rails.application.routes.draw do
     end
   end
   
-  resources :users
+  resources :users 
   resources :brands
   resources :categories, only: [:show]
   resources :goods,  only: [:new, :create, :show]
-  
+  resources :signup do
+    collection do
+      get 'step1'
+      get 'step2'
+      get 'step3'
+      get 'step4'
+      get 'done'
+    end
 end
