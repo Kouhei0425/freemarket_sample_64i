7a0a1d5c1b4e40cb9c33fda6c310f22975e130ce
hibiki20600 <hibiki_inobe@icloud.com>
2019-12-12 18:32:00 +0900

    toppage view

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,12 +1,14 @@
 class BuysController < ApplicationController
+
   def index
     @good = Good.find(params[:good_id])
   end
+
   def create
-    buy.create(buy_params)
+    Buy.create(buy_params)
   end
   private
   def buy_params
-    params.require(:buy).permit(:good_id, :user_id, :price)
+    params.require(:buy).permit(:good_id, :user_id, :price, buy_address_attribute: [:buy_address], buy_credit_attribute: [:buy_credit] )
   end
 end
--- app/models/buy.rb
+++ app/models/buy.rb
@@ -1,6 +1,9 @@
 class Buy < ApplicationRecord
   belongs_to :user
   has_one    :good
-  has_many   :buy_address
-  has_many   :buy_credit
+  has_one   :buy_address
+  has_one   :buy_credit
+  accepts_nested_attributes_for :buy_address
+  accepts_nested_attributes_for :buy_credit
+
 end
