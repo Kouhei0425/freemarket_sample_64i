58e55b1d4d8beca893ea52cc2b44ad00ac5e6602
Hiropey <hiropee.yam@gmail.com>
2019-12-13 19:11:09 +0900

    buys-controller
    WIP

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -4,12 +4,12 @@ class BuysController < ApplicationController
     @good = Good.find(params[:good_id])
     @address = Address.where( user_id: current_user.id)[0]
     @buy = Buy.new
-    @buy_address = @buy.buy_addresses.build
+    @buy.buy_addresses.build
   end
 
   def create
-    buy = Buy.create(buy_params)
-    if buy.save
+    @buy = Buy.create(buy_params)
+    if @buy.save
       BuyAddress.create(post: params[:buy_address][:post], prefecture: params[:buy_address][:prefecture], city: params[:buy_address][:city],
       address: params[:buy_address][:address], buil: params[:buy_address][:buil], buy_id: buy.id )
     end
