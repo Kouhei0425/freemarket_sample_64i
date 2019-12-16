97b857f3310e1ad1ea55615fa5acb0fbfcb26df9
Kouhei0425 <mk.ibra2.swd@ezweb.ne.jp>
2019-12-16 13:44:16 +0900

    a

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -20,12 +20,28 @@ class SignupController < ApplicationController
   end
 
   def step3
+    session[:phone_number] = user_params[:phone_number]
+    @user = User.new 
   end
 
   def step4
+    session[:post] = user_params[:post]
+    session[:prefecture] = user_params[:prefecture]
+    session[:city] = user_params[:city]
+    session[:address] = user_params[:address]
+    session[:buil] = user_params[:buil]
+    @user = User.new
+    @user.addresses.build
   end
 
   def done
+    session[:number] = user_params[:number]
+    session[:type] = user_params[:type]
+    session[:month] = user_params[:month]
+    session[:year] = user_params[:year]
+    session[:security] = user_params[:security]
+    @user = User.new
+    @user.credits.build
   end
   
   def create
@@ -41,14 +57,28 @@ class SignupController < ApplicationController
     first_kana: session[:first_kana],
     birsday_year: session[:birsday_year],
     birsday_month: session[:birsday_month],
-    birthday_date: session[:birthday_date]
+    birthday_date: session[:birthday_date],
+
+    phone_number: session[:phone_number],
+
+    post: session[:post],
+    prefecture: session[:prefecture],
+    city: session[:city],
+    address: session[:address],
+    buil: session[:buil],
+
+    number: user_params[:number],
+    type: user_params[:type],
+    month: user_params[:month],
+    year: user_params[:year],
+    security: user_params[:security]
     )
   end
   private
   
   def user_params
     params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :family_name, 
-      :first_name, :family_kana, :first_kana
+      :first_name, :family_kana, :first_kana, :birsday_year, :birsday_month, :birthday_date, :phone_number,address_attributes:[:post, :prefecture, :city, :address, :buil ],credits_attributes:[:number,:type,:month,:year,:security]
   )
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
   has_many :buys
   has_many :credits
   has_many :addresses
+  accepts_nested_attributes_for :addresses
+  accepts_nested_attributes_for :credits
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
