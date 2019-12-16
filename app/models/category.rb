705766bd44cbd592f8fff85da98562bd2afa8303
hibiki20600 <hibiki_inobe@icloud.com>
2019-12-16 10:53:06 +0900

    パンクず

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
   has_many :category_goods
   has_many :goods, through: :category_goods, dependent: :destroy
-  belongs_to :parent, class_name: "Category", optional: true
+  belongs_to :parent, class_name: "Category"
   has_many :children, class_name: "Category", foreign_key: :parent_id
 
   def self.search(keyword)
