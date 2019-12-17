a41754dfb717328715b9726c43b09b2cb95fabc7
Hiropey <hiropee.yam@gmail.com>
2019-12-17 18:16:49 +0900

    payjp WIP

--- db/migrate/20191211084743_create_credits.rb
+++ db/migrate/20191211084743_create_credits.rb
@@ -2,11 +2,11 @@ class CreateCredits < ActiveRecord::Migration[5.0]
   def change
     create_table :credits do |t|
       t.integer     :number,    null: false
-      t.string   :type,      null: false
+      t.string      :type,      null: false
       t.integer     :month,     null: false
       t.integer     :year,      null: false
-      t.string   :security,  null: false
-      t.references :user, null: false, foreign_key: true
+      t.string      :security,  null: false
+      t.references  :user, null: false, foreign_key: true
       t.timestamps
     end
   end
