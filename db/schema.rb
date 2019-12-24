adfbb5b433f05b68741197baf0a76e611eebbf87
hibiki20600 <hibiki_inobe@icloud.com>
2019-12-24 21:21:00 +0900

    profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191224120507) do
+ActiveRecord::Schema.define(version: 20191224121900) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "family_name",  null: false
@@ -138,22 +138,23 @@ ActiveRecord::Schema.define(version: 20191224120507) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "nick_name",                           null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "family_name",                         null: false
-    t.string   "first_name",                          null: false
-    t.string   "family_kana",                         null: false
-    t.string   "first_kana",                          null: false
-    t.integer  "birthday_year",                       null: false
-    t.integer  "birthday_month",                      null: false
-    t.integer  "birthday_date",                       null: false
-    t.string   "phone_number",                        null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "nick_name",                                         null: false
+    t.string   "email",                                default: "", null: false
+    t.string   "family_name",                                       null: false
+    t.string   "first_name",                                        null: false
+    t.string   "family_kana",                                       null: false
+    t.string   "first_kana",                                        null: false
+    t.integer  "birthday_year",                                     null: false
+    t.integer  "birthday_month",                                    null: false
+    t.integer  "birthday_date",                                     null: false
+    t.string   "phone_number",                                      null: false
+    t.string   "encrypted_password",                   default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
+    t.text     "profile",                limit: 65535
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
