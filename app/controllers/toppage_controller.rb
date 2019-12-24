d5481fc2c013b36b767dabd2678dbe09037224f8
hibiki20600 <hibiki_inobe@icloud.com>
2019-12-24 21:29:38 +0900

    created_by

--- app/controllers/toppage_controller.rb
+++ app/controllers/toppage_controller.rb
@@ -1,21 +1,21 @@
 class ToppageController < ApplicationController
   def index
     @ladies = Category.find(1)
-    @ladies_goods = @ladies.goods.first(10)
+    @ladies_goods = @ladies.goods.order("created_at DESC").first(10)
     @mens = Category.find(2)
-    @mens_goods = @mens.goods.first(10)
+    @mens_goods = @mens.goods.order("created_at DESC").first(10)
     @electronics = Category.find(8)
-    @electronics_goods = @electronics.goods.first(10)
+    @electronics_goods = @electronics.goods.order("created_at DESC").first(10)
     @hobbies = Category.find(6)
-    @hobbies_goods = @hobbies.goods.first(10)
+    @hobbies_goods = @hobbies.goods.order("created_at DESC").first(10)
     @chanel = Brand.find(1)
-    @chanel_goods = @chanel.goods.first(10)
+    @chanel_goods = @chanel.goods.order("created_at DESC").first(10)
     @louis = Brand.find(3)
-    @louis_goods = @louis.goods.first(10)
+    @louis_goods = @louis.goods.order("created_at DESC").first(10)
     @nike = Brand.find(2)
-    @nike_goods = @nike.goods.first(10)
+    @nike_goods = @nike.goods.order("created_at DESC").first(10)
     @supreme = Brand.find(4)
-    @supreme_goods = @supreme.goods.first(10)
+    @supreme_goods = @supreme.goods.order("created_at DESC").first(10)
   end
 
   def search
